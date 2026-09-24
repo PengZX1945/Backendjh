76003f4a6413bef7a175deb18b57eeba709b40d5
syc <sl20140231@163.com>
2026-09-24 13:02:40 +0800

    feat: add user registration service

--- internal/service/user.go
+++ internal/service/user.go
@@ -14,6 +14,32 @@ var usernameRe = regexp.MustCompile(`^[a-zA-Z0-9_]{4,32}$`)
 
 type UserService struct{ JWTSecret string }
 
+func (s *UserService) Register(username, password, nickname, contact string) (*model.User, *errcode.Error) {
+	if !usernameRe.MatchString(username) || len(password) < 8 || len(password) > 64 {
+		return nil, errcode.ParamError
+	}
+	if nickname == "" || contact == "" || len(nickname) > 32 {
+		return nil, errcode.ParamError
+	}
+	exists, err := repository.FindUserByUsername(username)
+	if err != nil {
+		return nil, errcode.InternalError
+	}
+	if exists != nil {
+		return nil, errcode.UsernameExists
+	}
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, errcode.InternalError
+	}
+	u := &model.User{Username: username, PasswordHash: string(hash), Nickname: nickname, Contact: contact, Role: model.RoleUser}
+
+	if err := repository.CreateUser(u); err != nil {
+		return nil, errcode.InternalError
+	}
+	return u, nil
+}
+
 func (s *UserService) Login(username, password string) (string, *model.User, *errcode.Error) {
 	u, err := repository.FindUserByUsername(username)
 	if err != nil {
