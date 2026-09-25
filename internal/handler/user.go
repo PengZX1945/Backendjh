fd107d78dd9f5fbf43a4a4e057d2b68fbfe63b27
syc <sl20140231@163.com>
2026-09-25 23:35:20 +0800

    feat: add user handler with login endpoint logic

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,1 +1,36 @@
 package handler
+
+import (
+	"Backendjh/internal/pkg/errcode"
+	"Backendjh/internal/pkg/response"
+	"Backendjh/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	userService *service.UserService
+}
+
+func NewUserHandler(u *service.UserService) *UserHandler {
+	return &UserHandler{userService: u}
+}
+
+type loginRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func (h *UserHandler) Login(c *gin.Context) {
+	var req loginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.Fail(c, errcode.ParamError)
+		return
+	}
+	token, _, err := h.userService.Login(req.Username, req.Password)
+	if err != nil {
+		response.Fail(c, err)
+		return
+	}
+	response.OK(c, gin.H{"token": token})
+}
